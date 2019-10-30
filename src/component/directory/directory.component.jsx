232087b8245c21e9410db8c530007a14cf23984e
gadfrey13 <gadfrey209@gmail.com>
2019-10-30 04:49:57 -0700

    broken homepage to smaller components

---
+++ src/component/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import data from './directory.data';
+import './directory.styles.scss';
+
+import MenuItem from '../menu-item/menu-item.component';
+class Directory extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            section: data
+        }
+    }
+
+    render(){
+        return (
+           <div className="directory-menu">
+                {
+                    this.state.section.map(({title,imageUrl,id, size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    ))
+                }
+           </div> 
+        )
+    }
+}
+
+export default Directory;
