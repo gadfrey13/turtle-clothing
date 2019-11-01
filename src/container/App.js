c186b39236b5eeb1dd4a0f35185fee3674f54794
gadfrey13 <gadfrey209@gmail.com>
2019-11-01 12:14:20 -0700

    added the ability to add users to the database and auntheicate users using google sign in

--- src/container/App.js
+++ src/container/App.js
@@ -5,7 +5,7 @@ import HomePage from "../component/pages/homepage/homepage.component";
 import ShopPage from "../component/pages/shop/shop.component";
 import Header from "../component/header/header.component";
 import SignInAndSignUpPage from "../component/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "../firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -18,8 +18,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+       if(userAuth){
+         const userRef = await createUserProfileDocument(userAuth);
+
+         userRef.onSnapshot(snapShot => {
+           this.setState({
+             currentUser: {
+               id: snapShot.id,
+               ...snapShot.data()
+             }
+           })
+         })
+       }
+
+       this.setState({currentUser: userAuth});//signout currentuser becomes null
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,25 +1,54 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-    apiKey: "AIzaSyACbbCmJkFrubdoaUdW4Mpwk1yi1MZdZrQ",
-    authDomain: "turtle-db.firebaseapp.com",
-    databaseURL: "https://turtle-db.firebaseio.com",
-    projectId: "turtle-db",
-    storageBucket: "turtle-db.appspot.com",
-    messagingSenderId: "896768624900",
-    appId: "1:896768624900:web:592bcfcd0b5d2843bda851",
-    measurementId: "G-45RTQ3S797"
-  };
+  apiKey: "AIzaSyACbbCmJkFrubdoaUdW4Mpwk1yi1MZdZrQ",
+  authDomain: "turtle-db.firebaseapp.com",
+  databaseURL: "https://turtle-db.firebaseio.com",
+  projectId: "turtle-db",
+  storageBucket: "turtle-db.appspot.com",
+  messagingSenderId: "896768624900",
+  appId: "1:896768624900:web:592bcfcd0b5d2843bda851",
+  measurementId: "G-45RTQ3S797"
+};
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    //if false return
+    return;
+  }
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+  const userRef = firestore.doc(`users/${userAuth.uid}`); //check if user exist in the database
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt: 'select_account'})
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+  const snapShot = await userRef.get();//get the snapshot 
 
-  export default firebase;
+  if (!snapShot.exists) {//if false
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (err) {
+        console.log('error creating user', err.message);
+    }
+  }
+
+  return userRef;
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
