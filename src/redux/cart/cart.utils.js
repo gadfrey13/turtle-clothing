2a332579a37cd8b407bfac108f885a0057e819c7
gadfrey13 <gadfrey209@gmail.com>
2019-11-05 13:27:28 -0800

    updated shop data and added utils

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,15 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>//returns a new array so components rerender. If property change only component does not rerender
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, {...cartItemToAdd, quantity: 1}];//quanity property gets attached the first item is added to cart
+};
