cf3c0df530835d787a9ea1219a042a728cdfc2e6
gadfrey13 <gadfrey209@gmail.com>
2019-11-05 09:59:00 -0800

    added the toggle function to dropdown cart

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -3,13 +3,16 @@ import "./header.styles.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
-}) 
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
+});
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -32,11 +35,11 @@ const Header = ({ currentUser }) => {
             SIGNIN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropDown />}
     </div>
   );
 };
-
-
 
 export default connect(mapStateToProps)(Header);
